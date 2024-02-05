e51b32a43ac465bf460ebc8a637ab2dcf0287770
tanaya1312 <152295427+tanaya1312@users.noreply.github.com>
2024-02-05 11:28:18 +0530

    Delete Update_Department.py

--- Update_Department.py
+++
@@ -1,6 +0,0 @@
-from Main import Connection
-Cursor=Connection.cursor()
-
-Update="UPDATE Department SET Dept_Name='Deposits' WHERE Dept_Name='Cash Management'"
-Cursor.execute(Update)
-Connection.commit()
