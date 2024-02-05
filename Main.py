866649704bd8e0d76fa45a306deeda96286b8455
Ajay4507 <152296020+Ajay4507@users.noreply.github.com>
2024-02-05 11:15:15 +0530

    Add Main

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import mysql.connector
-Connection = mysql.connector.connect (
+mydb= mysql.connector.connect(
     host='localhost',
     user='root',
-    password='Tanaya@1312',
-    database='bank'
-)
+    password='Ajay@2002',
+    database='Bank')
+
